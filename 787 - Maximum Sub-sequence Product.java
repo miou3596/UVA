29e3d0a5690369941037851d41c314947ba68b67
omaryasser <oyaraouf@gmail.com>
2017-03-31 15:08:07 +0200

    Create 787 - Maximum Sub-sequence Product.java

---
+++ 787 - Maximum Sub-sequence Product.java
@@ -0,0 +1,141 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		while(sc.ready()) {
+			ArrayList<Integer> arr = new ArrayList<>();
+			int num;
+			while((num = sc.nextInt()) != -999999)
+				arr.add(num);
+			
+			BigInteger res = BigInteger.valueOf(arr.get(0));
+			int n = arr.size();
+			for (int i = 0; i < n; i++) {
+				BigInteger cur = BigInteger.valueOf(arr.get(i));
+				res = res.max(cur);
+				for (int j = i + 1; j < n; j++) {
+					cur = cur.multiply(BigInteger.valueOf(arr.get(j)));
+					res = res.max(cur);
+				}
+			}
+			out.println(res);
+		}
+		
+		out.flush();
+		out.close();
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	static class Scanner {
+
+		StringTokenizer st;
+		BufferedReader br;
+
+		public Scanner(InputStream s) {
+			br = new BufferedReader(new InputStreamReader(s));
+		}
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens())
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		}
+
+		public String nextLine() throws IOException {
+			return br.readLine();
+		}
+
+		public double nextDouble() throws IOException {
+			String x = next();
+			StringBuilder sb = new StringBuilder("0");
+			double res = 0, f = 1;
+			boolean dec = false, neg = false;
+			int start = 0;
+			if (x.charAt(0) == '-') {
+				neg = true;
+				start++;
+			}
+			for (int i = start; i < x.length(); i++)
+				if (x.charAt(i) == '.') {
+					res = Long.parseLong(sb.toString());
+					sb = new StringBuilder("0");
+					dec = true;
+				} else {
+					sb.append(x.charAt(i));
+					if (dec)
+						f *= 10;
+				}
+			res += Long.parseLong(sb.toString()) / f;
+			return res * (neg ? -1 : 1);
+		}
+
+		public boolean ready() throws IOException {
+			return br.ready();
+		}
+
+		public boolean nextEmpty() throws IOException {
+			String s = nextLine();
+			st = new StringTokenizer(s);
+			return s.isEmpty();
+		}
+	}
+}
