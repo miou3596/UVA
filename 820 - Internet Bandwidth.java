a2e19705c6ef3b25753e8fc31df0c44ae62c2a57
omaryasser <oyaraouf@gmail.com>
2017-08-06 13:32:20 +0200

    Update 820 - Internet Bandwidth.java

--- 820 - Internet Bandwidth.java
+++ 820 - Internet Bandwidth.java
@@ -1,88 +1,79 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-
-    static final int INF = (int)1e9;
-    static int V, s, t;						//input
-    static ArrayList<Integer>[] adjList;	//input
-    static int[][] res;						//input
-    static int[] p;
-
-    static int augment(int v, int flow)
-    {
-        if(v == s)
-            return flow;
-        flow =  augment(p[v], Math.min(res[p[v]][v],flow));
-        res[p[v]][v] -= flow;
-        res[v][p[v]] += flow;
-
-        return flow;
-    }
-
-    static int edmondsKarp()
-    {
-        int mf = 0;
-        while(true)
-        {
-            Queue<Integer> q = new LinkedList<Integer>();
-            p = new int[V];
-            Arrays.fill(p, -1);
-            p[s] = s;
-            q.add(s);
-            while(!q.isEmpty())
-            {
-                int u = q.remove();
-                if(u == t)
-                    break;
-                for(int v: adjList[u])
-                    if(res[u][v] > 0 && p[v] == -1)
-                    {
-                        p[v] = u;
-                        q.add(v);
-                    }
-            }
-
-            if(p[t] == -1)
-                break;
-            mf += augment(t, INF);
-
-        }
-
-
-
-        return mf;
-    }
-
-    public static void main (String [] args) throws IOException {
-
+    static int INF = (int)1e9;
+    static int V, s, t, c[][];
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        for (int T = 1; ; T ++)
-        {
+
+        for (int tc = 1;; tc++){
             V = sc.nextInt();
             if (V == 0) break;
-            s = sc.nextInt() - 1; t = sc.nextInt() - 1; int E = sc.nextInt();
-            adjList = new ArrayList[V];
-            res = new int [V][V];
-            for (int i = 0 ; i < V ; ++i)
-                adjList[i] = new ArrayList<>();
-            while (E -- >0)
-            {
-                int u = sc.nextInt() - 1 , v = sc.nextInt() - 1 , w = sc.nextInt();
-                adjList[u].add(v);
-                adjList[v].add(u);
-                res[u][v] += w ;
-                res[v][u] += w ;
+            s = sc.nextInt() - 1;
+            t = sc.nextInt() - 1;
+            int E = sc.nextInt();
+            c = new int[V][V];
+            while (E-- > 0) {
+                int u = sc.nextInt() - 1, v = sc.nextInt() - 1, w = sc.nextInt();
+                c[u][v] += w;
+                c[v][u] += w;
             }
-            out.printf("Network %d\n" , T);
-            out.printf("The bandwidth is %d.\n" , edmondsKarp());
-            out.print("\n");
+
+            out.printf("Network %d\n" +
+                    "The bandwidth is %d.\n\n", tc, pushRelabel());
         }
-        out.flush();
+
         out.close();
     }
 
+    static int pushRelabel () {
+        int[] h = new int[V], e = new int[V], f[] = new int[V][V];
+        h[s] = V - 1;
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] isActive = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            f[i][s] = -(f[s][i] = e[i] = c[s][i]);
+            if (i != s && i != t && e[i] > 0) {
+                isActive[i] = true;
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int u = q.peek();
+            boolean pushed = false;
+            for (int v = 0; v < V && e[u] != 0; v++) {
+                if (h[u] > h[v] && c[u][v] - f[u][v] > 0) {
+                    int df = Math.min(e[u], c[u][v] - f[u][v]);
+                    f[u][v] += df;
+                    f[v][u] -= df;
+                    e[u] -= df;
+                    e[v] += df;
+                    if (v != s && v != t && !isActive[v]) {
+                        isActive[v] = true;
+                        q.add(v);
+                    }
+                    pushed = true;
+                }
+            }
+            if (e[u] == 0) {
+                isActive[u] = false;
+                q.remove();
+            }
+
+            if (!pushed) {
+                h[u] = INF;
+                for (int v = 0; v < V; ++v)
+                    if (c[u][v] - f[u][v] > 0 && h[v] <= h[u])
+                        h[u] = h[v] + 1;
+            }
+        }
+        return e[t];
+    }
 
 
 
@@ -119,62 +110,39 @@ public class Main {
 
 
 
-
-
-
-
-
-    static class Scanner
-    {
+    static class Scanner {
         StringTokenizer st;
         BufferedReader br;
 
-        public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+        public Scanner(InputStream s) {
+            br = new BufferedReader(new InputStreamReader(s));
+        }
+        public Scanner(FileReader s) {
+            br = new BufferedReader(s);
+        }
 
-        public String next() throws IOException
-        {
-            while (st == null || !st.hasMoreTokens())
+        public String next() {
+            while (st == null || !st.hasMoreTokens()) try {
                 st = new StringTokenizer(br.readLine());
+            } catch (Exception e) {
+            }
             return st.nextToken();
         }
 
-        public int nextInt() throws IOException {return Integer.parseInt(next());}
-
-        public long nextLong() throws IOException {return Long.parseLong(next());}
-
-        public String nextLine() throws IOException {return br.readLine();}
-
-        public double nextDouble() throws IOException
-        {
-            String x = next();
-            StringBuilder sb = new StringBuilder("0");
-            double res = 0, f = 1;
-            boolean dec = false, neg = false;
-            int start = 0;
-            if(x.charAt(0) == '-')
-            {
-                neg = true;
-                start++;
-            }
-            for(int i = start; i < x.length(); i++)
-                if(x.charAt(i) == '.')
-                {
-                    res = Long.parseLong(sb.toString());
-                    sb = new StringBuilder("0");
-                    dec = true;
-                }
-                else
-                {
-                    sb.append(x.charAt(i));
-                    if(dec)
-                        f *= 10;
-                }
-            res += Long.parseLong(sb.toString()) / f;
-            return res * (neg?-1:1);
+        public String nextLine() throws IOException {
+            return br.readLine();
         }
 
-        public boolean ready() throws IOException {return br.ready();}
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
 
+        public boolean ready() throws IOException {
+            return br.ready();
+        }
     }
 }
