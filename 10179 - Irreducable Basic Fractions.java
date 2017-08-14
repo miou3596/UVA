7ee01981b91f88eab476abfb694ef69001363eef
omaryasser <oyaraouf@gmail.com>
2017-08-14 19:52:43 +0200

    Update 10179 - Irreducable Basic Fractions.java

--- 10179 - Irreducable Basic Fractions.java
+++ 10179 - Irreducable Basic Fractions.java
@@ -1,56 +1,54 @@
 import java.io.*;
 import java.util.*;
 
-
+/**
+ * Created by omar on 13/08/17.
+ */
 public class Main {
-
-    static int MAX = 31623 ;
-    static int primes [] = new int[MAX  +1];
-    static int N = 0;
-
-    static void seive ()
+    static ArrayList<Integer> primes;
+    public static void seive (int n)
     {
-        boolean isComposite [] = new boolean[MAX  +1];
-        isComposite[0] = isComposite[1] = true;
-        for (int i = 2 ; i <= MAX ; ++i)
-            if (!isComposite[i])
-            {
-                primes[N++] = i;
-                for (int j = i + i ; j <= MAX  ; j += i)
-                    isComposite[j] = true;
-            }
+        primes = new ArrayList<>();
+        boolean primess [] = new boolean[n+1];
+        Arrays.fill(primess, true);
+        primess[0]=primess[1]=false;
+        for(int i =0; i*i<=n; i++)
+            if(primess[i])
+                for(int j =2; 1l * i*j <=n; j++)
+                    primess [i*j]=false;
+
+        for (int i = 0; i <= n; ++i)
+            if (primess[i]) primes.add(i);
     }
 
-    static long eulerphi (int n)
+    public static long num_of_coprimes(long N)
     {
-        long ans = n ;
-        for (int PF_IDX = 0 , PF = 2 ; PF * PF <= n ; PF = primes[++PF_IDX])
+        seive((int)Math.ceil(Math.sqrt(N)) + 1);
+        int PF_IDX = 0;
+        long PF = 2;
+        long ans = N;
+        while (PF * PF <= N)
         {
-            if (n % PF == 0)
-            {
-                ans -= ans / PF ;
-                while (n % PF == 0) {n /= PF;}
-            }
-            if (PF_IDX == N - 1) break;
+            if (N % PF == 0) ans -= ans / PF;
+            while (N % PF == 0) N /= PF;
+            if (PF_IDX == primes.size() - 1) break;
+            PF = primes.get(++PF_IDX);
         }
-        if (n != 1)
-            ans -= ans / n ;
-        return ans ;
+
+        if (N != 1) ans -= ans / N;
+        return ans;
     }
-    public static void main(String [] args) throws  Exception
-    {
+
+    public static void main (String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        StringBuilder sb = new StringBuilder();
-        seive();
-        while (1 == 1)
-        {
+
+        seive((int)1e5);
+        while (true) {
             int n = sc.nextInt();
             if (n == 0) break;
-
-            out.printf("%d\n" , eulerphi (n));
+            out.println(num_of_coprimes(n));
         }
-        out.flush();
         out.close();
     }
 
@@ -90,57 +88,81 @@ public class Main {
 
 
 
-    static class Scanner
-    {
+
+
+
+
+
+    static class Scanner {
+
+
         StringTokenizer st;
         BufferedReader br;
 
-        public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+        public Scanner(InputStream s) {
+            br = new BufferedReader(new InputStreamReader(s));
+        }
 
-        public String next() throws IOException
-        {
+        public Scanner(FileReader f) {
+            br = new BufferedReader(f);
+        }
+
+        public String next() {
             while (st == null || !st.hasMoreTokens())
-                st = new StringTokenizer(br.readLine());
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                }
             return st.nextToken();
         }
 
-        public int nextInt() throws IOException {return Integer.parseInt(next());}
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
 
-        public long nextLong() throws IOException {return Long.parseLong(next());}
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
 
-        public String nextLine() throws IOException {return br.readLine();}
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch (Exception e) {
+                return null;
+            }
+        }
 
-        public double nextDouble() throws IOException
-        {
+        public double nextDouble() {
             String x = next();
             StringBuilder sb = new StringBuilder("0");
             double res = 0, f = 1;
             boolean dec = false, neg = false;
             int start = 0;
-            if(x.charAt(0) == '-')
-            {
+            if (x.charAt(0) == '-') {
                 neg = true;
                 start++;
             }
-            for(int i = start; i < x.length(); i++)
-                if(x.charAt(i) == '.')
-                {
+            for (int i = start; i < x.length(); i++)
+                if (x.charAt(i) == '.') {
                     res = Long.parseLong(sb.toString());
                     sb = new StringBuilder("0");
                     dec = true;
-                }
-                else
-                {
+                } else {
                     sb.append(x.charAt(i));
-                    if(dec)
+                    if (dec)
                         f *= 10;
                 }
             res += Long.parseLong(sb.toString()) / f;
-            return res * (neg?-1:1);
+            return res * (neg ? -1 : 1);
         }
 
-        public boolean ready() throws IOException {return br.ready();}
-
-
+        public boolean ready() {
+            try {
+                return br.ready();
+            } catch (Exception e) {
+                return false;
+            }
+        }
     }
+
 }
